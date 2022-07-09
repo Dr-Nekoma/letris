9b277d89128f1005c62df34e0ec2cee43f93655e
Eduardo Lemos Rocha <dudulr10@gmail.com>
2022-07-08 23:20:37 -0300

    Added first draft of the game kinda working

--- logic.lisp
+++ logic.lisp
@@ -118,7 +118,6 @@
         (setf (representation piece) i-state-2)
         (setf (representation piece) i-state-1))))
 
-
 (defun rotate-piece (piece)
   "Rotation of pieces. Two special cases are considered."
   (case (kind piece)
@@ -128,11 +127,12 @@
   piece)
 
 (defun has-collision (board piece)
+  (declare (optimize (debug 3) (safety 3)))
   (destructuring-bind (piece-row-size piece-column-size) (array-dimensions (representation piece))
     (destructuring-bind (board-row-size board-column-size) (array-dimensions board)
       (let* ((initial-position (pos piece))
-             (piece-pos-x (car initial-position))
-             (piece-pos-y (cadr initial-position))
+             (piece-pos-x (first initial-position))
+             (piece-pos-y (second initial-position))
              (piece-matrix (representation piece))
              (answer nil))
         (loop :for i :below piece-row-size
@@ -140,10 +140,15 @@
                         :do (let* ((piece-value (aref piece-matrix i j))
                                    (board-x (+ i piece-pos-x))
                                    (board-y (+ j piece-pos-y))
-                                   (is-out-of-bounds (or (>= board-x board-row-size) (>= board-y board-column-size)))
+                                   (is-out-of-bounds (or (>= board-x board-row-size)
+                                                         (>= board-y board-column-size)
+                                                         (< board-y 0)))
                                    (is-piece-1 (= piece-value 1)))
+                              (terpri)
                               (if is-out-of-bounds
-                                  (setf answer (or answer is-piece-1))
+                                  (progn
+                                    (terpri)
+                                    (setf answer (or answer is-piece-1)))
                                   (let ((board-value (aref board (+ i piece-pos-x) (+ j piece-pos-y))))
                                     (setf answer (or answer (and (= board-value 1) is-piece-1))))))))
         answer))))
@@ -167,15 +172,8 @@
 ;; Given a piece, a board, and an user-input, can we proceed with this movement?
 
 (defparameter *test-piece* (spawn 'o-piece))
-(setf (pos *test-piece*) '(0 2))
 
-(defparameter *test-board* (make-array '(6 6)
-                                       :initial-contents #(#(0 0 0 0 0 1)
-                                                           #(0 0 0 0 0 1)                                                    
-                                                           #(0 0 0 0 0 1)
-                                                           #(0 0 0 0 0 1)
-                                                           #(0 0 0 0 0 1)
-                                                           #(0 0 0 0 0 1))))
+(defparameter *test-board* (make-board))
 
 (defun copy-multi-array (array)
   (let ((res (make-board)))
@@ -183,15 +181,36 @@
           :do (setf (row-major-aref res i) (row-major-aref array i)))
     res))
 
+;; Read user movement
+;; Attempt to move the piece according the move
+;; If we collide going down, we glue the piece onto the board, we generate a new piece
+;; Else, we should attempt to go down
+;; If we collide, we glue the piece onto the board, we generate a new piece
+;; We draw
+;; We repeat
+
 (defun game-loop ()
-  (let ((user-movement (read-char))  
-        (clone-board (copy-multi-array *test-board*)))
-    (case user-movement
-      (#\d (princ "👉")) ;(move-piece-right clone-board *test-piece*))
-      (#\a (princ "👈")) ;(move-piece-left clone-board *test-piece*))
-      (#\w (princ "🌀")) ;(rotate-piece *test-piece*))
-      (#\s (princ "👇"))) ;(advance-game-tick clone-board *test-piece*)))
-    clone-board))
+  (let* ((user-movement (read-char))
+         (success (attempt-to-move *test-board* *test-piece* user-movement)))
+    (clear-input)
+    (if (and (null success) (eql user-movement #\s))
+        (progn
+          (glue-piece-on-board *test-board* *test-piece*)
+          (setf *test-piece* (spawn-random-piece)))
+        (progn
+          (setf success (attempt-to-move *test-board* *test-piece* #\s))
+          (when (null success)
+            (glue-piece-on-board *test-board* *test-piece*)
+            (setf *test-piece* (spawn-random-piece)))))
+    (draw-piece-on-board *test-board* *test-piece*)
+    (game-loop)))
+
+
+;; Pick the user movement and move
+;; Check for collision and glue if necessary
+;; Go down
+;; Check for collision and glue if necessary
+;; Repeat
 
 ;; We know we want to go to the right
 ;; We should update the piece's pos to x + 1 and y + 0
@@ -199,22 +218,36 @@
 ;; If we get a collision we just return the original board
 ;; If we don't get a collision we just draw the updated piece in the board to get a new one
 
-(defun move-adjustments (piece board movement-coord)
-  (let ((new-piece (copy piece)))
-    (setf (pos new-piece) movement-coord)
-    (unless (has-collision board new-piece) (draw-piece-on-board board new-piece))))
+(defun move-adjustments (piece board func)
+  (let ((new-piece (funcall func (copy piece))))
+    (if (has-collision board new-piece)
+        nil
+        (progn
+          (setf (pos piece) (pos new-piece)
+                (representation piece) (representation new-piece))
+          t))))
+
+(defun change-coords (coords)
+  (lambda (piece)
+    (setf (pos piece) coords)
+    piece))
 
 (defun attempt-to-move (board piece user-movement)
   (let ((x (first (pos piece)))
         (y (second (pos piece))))
     (case user-movement
-      (#\d (move-adjustments piece board `(,x ,(+ y 1)))) ;(move-piece-right clone-board *test-piece*))
-      (#\a (move-adjustments piece board `(,x ,(- y 1)))) ;(move-piece-left clone-board *test-piece*))
-      (#\w (move-adjustments (rotate-piece piece) board `(,x ,y))) ;(rotate-piece *test-piece*))
-      (#\s (move-adjustments piece board `(,(+ x 1) ,y))))
-    board))
+      (#\d (move-adjustments piece board (change-coords `(,x ,(+ y 1)))))
+      (#\a (move-adjustments piece board (change-coords `(,x ,(- y 1)))))
+      (#\w (move-adjustments piece board  #'rotate-piece))
+      (#\s (move-adjustments piece board (change-coords `(,(+ x 1) ,y)))))))
 
 (defun draw-piece-on-board (board piece)
+  (let ((board-clone (copy-multi-array board)))
+    (glue-piece-on-board board-clone piece)
+    (prin1 board-clone)
+    (terpri)))
+
+(defun glue-piece-on-board (board piece)
   (let ((init-pos-x (first (pos piece)))
         (init-pos-y (second (pos piece)))
         (dimension (array-dimension (representation piece) 0))
@@ -226,8 +259,9 @@
                     :do (when (array-in-bounds-p board i j)
                           (let ((board-value (aref board i j))
                                 (piece-value (aref piece-matrix a b)))
-                            (format t "~a ~a ~a ~a~%" i j a b)
                             (setf (aref board i j) (logior board-value piece-value))))))))
+
+
 
 ;; TODO: Treat user input
 ;; TODO: We need to clean the ghosts of the piece after moving (go to the UNLESS Nathan hates)
