79daa3e85bf7b5d3c64be380fa91524f1e5ed923
EduardoLR10 <dudulr10@gmail.com>
2023-06-04 18:10:18 -0300

    Add draft of nix configuration for letris

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Letris configuration";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+        {
+          devShells.default = pkgs.mkShell {
+            buildInputs = [
+              pkgs.sbcl
+              pkgs.xorg.libX11
+              pkgs.xorg.libX11.dev
+              pkgs.libGL
+            ];
+            shellHook = ''
+                export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath([pkgs.xorg.libX11])}:${pkgs.lib.makeLibraryPath([pkgs.xorg.libX11.dev])}:${pkgs.lib.makeLibraryPath([pkgs.libGL])}
+            '';      
+          };
+        }
+      );
+}
